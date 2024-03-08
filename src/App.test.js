18c4bf1c1913f6ebc0ef189fc7bee8c993655b98
kanchan sharma <kanchan4596@gmail.com>
2024-03-08 20:56:02 +0530

    app testing message

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
+  // testing the app
   const linkElement = screen.getByText(/about/i);
   expect(linkElement).toBeInTheDocument();
 });
